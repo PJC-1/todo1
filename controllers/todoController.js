629ff3c4ab10ca5719f661046c289637900584f6
PJC-1 <jumpzine@gmail.com>
2017-07-23 15:34:29 -0700

    connected to the db and tested creating an tiem

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,4 +1,19 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+
+// connect to the database
+
+// create a schema - this is like a blueprint
+var todoSchema = new mongoose.Schema({
+    item: String
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+// create test item to save to the db
+var itemOne = Todo({item: 'buy flowers'}).save(function(err){
+    if (err) throw err;
+    console.log('item saved');
+});
 
 var data = [{item: 'get milk'}, {item: 'walk dog'}, {item: 'kick some coding ass'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
